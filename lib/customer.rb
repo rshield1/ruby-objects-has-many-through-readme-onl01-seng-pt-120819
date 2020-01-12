209861016517f84e1e453e69bc905d49aa9e4f42
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-12 01:11:56 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,5 +16,11 @@ class Customer
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
+  
+  def waiters
+    meals.map do |meal|
+    meal.waiter
+  end
+end
  
 end
